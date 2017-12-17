a70bc68b67fa3232515e2503e45ee93e5fc3819c
Gary Moore <garyprm@outlook.com>
2017-12-17 22:46:24 +0000

    Update vm script

--- AzurePowershellAutomationScripts/Vm/Create-WindowsVM.ps1
+++ AzurePowershellAutomationScripts/Vm/Create-WindowsVM.ps1
@@ -42,3 +42,12 @@ $nic = New-AzureRmNetworkInterface -Name myNic1 -ResourceGroupName $resourceGrou
 $cred = Get-Credential -Message "Enter a username and password for the virtual machine."
 
 New-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
+
+# Create a virtual machine configuration
+$vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize Standard_D1 |
+  Set-AzureRmVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred |
+  Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest |
+  Add-AzureRmVMNetworkInterface -Id $nic.Id
+
+# Create a virtual machine
+New-AzureRmVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
